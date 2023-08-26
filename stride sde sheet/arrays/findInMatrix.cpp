9931de8b824ed95dd428a2417d9f85004246b303
aritrika ghosh <aritrika.ghosh.jobs@gmail.com>
2023-08-26 20:58:34 +0530

    optimized solutions

--- stride sde sheet/arrays/findInMatrix.cpp
+++ stride sde sheet/arrays/findInMatrix.cpp
@@ -59,3 +59,28 @@ public:
         
     }
 };
+
+/*OPTIMIZED O(LOG(m*n))*/
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+     int row=matrix.size();
+        int col=matrix[0].size();
+       int start=0, end=(row*col)-1;
+       
+      
+    while(start<=end){
+        int mid=(start+end)/2;
+        int n=mid/col;
+        int m=mid%col;
+       if(matrix[n][m]==target)return true;
+       if(matrix[n][m]>target)end=mid-1;
+       else start=mid+1;
+
+    }
+
+    return false;
+    }
+
+};
